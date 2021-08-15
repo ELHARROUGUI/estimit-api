14d694419d7205bffd0bb816891ac72b1a40ce95
El-Harrougui MOHAMED <elharrougui0@gmail.com>
2021-08-15 21:38:34 +0100

    Change db config using Qovery database

--- src/app/config/db.config.js
+++ src/app/config/db.config.js
@@ -1,8 +1,21 @@
-module.exports = {
+/* module.exports = {
   HOST: "remotemysql.com",
   USER: "UGaqbZpqLe",
   PASSWORD: "dItcZ0B4iy",
   DB: "UGaqbZpqLe",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+}; */
+module.exports = {
+  HOST: "z171c115a-mysql.qovery.io",
+  USER: "root",
+  PASSWORD: "76OxsLnl8iIRx5Fu",
+  DB: "estimitdb",
   dialect: "mysql",
   pool: {
     max: 5,
